5773c2a4779f3a10fdbf27d4deef06e127be7124
Ousepsony <ousepsony@gmail.com>
2022-05-10 21:00:33 +0530

    emptied index.js

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-// var HTMLCalculatorHolder = document.querySelector(".calculator-holder");
 
-// window.addEventListener("DOMContentLoaded",pageLoad);
-// window.addEventListener("resize",pageresize);
-
-// function pageLoad(){
-//     HTMLCalculatorHolder.style.height = window.innerHeight + " px";
-// }
-
-// function pageresize(){
-//     console.log(window.innerHeight);
-//     HTMLCalculatorHolder.style.minHeight = window.innerHeight + " px";
-//     HTMLCalculatorHolder.style.minHeight = window.innerHeight + " px";
-
-// }
